d15ad4bc28599fb2c5db6eac5e4716d5b8170fda
ctg31 <ctg31@uclive.ac.nz>
2019-05-14 21:01:48 +1200

    Basic setup of application to allow for easy expansion as the project gets developed.

---
+++ src/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    Home Page
+  </div>
+</template>
+
+
+<script>
+    export default {
+        data() {
+          return {
+            error: "",
+            errorFlag: false
+          }
+        },
+      mounted: function() {
+
+      },
+      methods: {
+
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Home from './Home.vue';
+import Vue from 'vue';
+import App from './App.vue';
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+
+const routes = [
+  {
+    path: "/",
+    component: Home
+  }
+];
+
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+});
+
+
 
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
-})
+});
