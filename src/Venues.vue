b4efd07a7f0bf234900534ffe7e7cf769127e5d4
ctg31 <ctg31@uclive.ac.nz>
2019-05-16 16:42:14 +1200

    Started working on venues. Created initial api request to get all the venues to have an initial display.

--- src/Venues.vue
+++ src/Venues.vue
@@ -1,15 +1,50 @@
 <template>
     <div>
-      Venues
+      Venues (here so I can easily distingiush)
+      <div class="venueFilters">
+        This is where filter options will be
+      </div>
+      <div v-for="venue in venues">
+        <div class="venueBox">
+          <div class="venueImage">
+
+          </div>
+          <div>
+            {{ venue.venueName }}
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Venues"
+        data() {
+          return {
+            error: "",
+            errorFlag: "",
+            venues: []
+          }
+        },
+      mounted: function() {
+        this.getVenues();
+      },
+      methods: {
+        getVenues: function() {
+          this.$http.get("http://localhost:4940/api/v1/venues")
+            .then(function(response) {
+              console.log(response);
+              this.venues = response.body;
+            }, function(error) {
+              console.log(error);
+            });
+        }
+      }
     }
 </script>
 
 <style scoped>
-
+  .venueBox {
+    height: 10rem;
+  }
 </style>
